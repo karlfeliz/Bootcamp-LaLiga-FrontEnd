8b5c6bb7c89546a6e02e36e9a59e8beb6c947beb
Daniel Fernandez <dfernandezcode@gmail.com>
2023-06-17 11:35:39 +0200

    header base

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,13 @@
+import "./Header.scss";
+import { NavLink } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1>| FOOTBALL MANAGER </h1>
+      <button>LOGIN/REGISTRO</button>
+    </header>
+  );
+};
+
+export default Header;
